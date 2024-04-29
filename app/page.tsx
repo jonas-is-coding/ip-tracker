547c516acbe19a568152a98c8772c2a0dbe6a3ed
Jonas Brahmst <jonasbrahmst@gmail.com>
2024-04-29 10:13:37 +0200

    Commit

--- app/page.tsx
+++ app/page.tsx
@@ -11,21 +11,15 @@ export default function Home() {
   const [lat, setLat] = useState("");
   const [lon, setLon] = useState("");
   const [host, setHost] = useState('');
-  const [ zipCode, setZipCode] = useState("");
+  const [zipCode, setZipCode] = useState("");
 
   useEffect(() => {
     const fetchIP = async () => {
       try {
-        const response = await fetch("http://ip-api.com/json/?fields=61439");
+        const response = await fetch("https://api64.ipify.org?format=json");
         const data = await response.json();
-        setIp(data.query);
-        setCity(data.city);
-        setCountry(data.country);
-        setLat(data.lat);
-        setLon(data.lon);
-        setHost(data.isp);
-        setZipCode(data.zip);
-        console.log(data)
+        setIp(data.ip);
+        console.log(data);
       } catch (error) {
         console.error("Error fetching IP:", error);
       }
@@ -43,38 +37,21 @@ export default function Home() {
     <main className="bg-black h-screen overflow-hidden">
       <NavBar />
       <div className="flex items-center justify-between w-full h-screen px-20 pt-20 gap-10">
-      <div className="w-full h-[600px]">
-        <Video />
-      </div>
-      <div className="w-[450px] text-center flex flex-col items-start justify-start gap-10">
-        <h2 className="text-white text-4xl font-semibold">Your information:</h2>
-        <ul className="h-full w-full text-white flex flex-col items-start gap-5 px-6">
-          <li className="text-left w-full text-xl">
-            Ip Adress: {ip}
-          </li>
-          <li className="text-left w-full text-xl">
-            City: {city}
-          </li>
-          <li className="text-left w-full text-xl">
-            Zip-Code: {zipCode}
-          </li>
-          <li className="text-left w-full text-xl">
-            Country: {country}
-          </li>
-          <li className="text-left w-full text-xl">
-            N: {lat}
-          </li>
-          <li className="text-left w-full text-xl">
-          W: {lon}
-          </li>
-          <li className="text-left w-full text-xl">
-            Host: {host}
-          </li>
-        </ul>
-        <Button variant={"outline"} className="w-full text-xl" size={"lg"} onClick={() => copyText()}>
-          Commit your fault 🤓
-        </Button>
-      </div>
+        <div className="w-full h-[600px]">
+          <Video />
+        </div>
+        <div className="w-[450px] text-center flex flex-col items-start justify-start gap-10">
+          <h2 className="text-white text-4xl font-semibold">Your information:</h2>
+          <ul className="h-full w-full text-white flex flex-col items-start gap-5 px-6">
+            <li className="text-left w-full text-xl">
+              Ip Adress: {ip}
+            </li>
+            {/* Andere Informationen hier anzeigen */}
+          </ul>
+          <Button variant={"outline"} className="w-full text-xl" size={"lg"} onClick={copyText}>
+            Commit your fault 🤓
+          </Button>
+        </div>
       </div>
     </main>
   );
